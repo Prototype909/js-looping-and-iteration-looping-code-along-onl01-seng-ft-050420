01065371ae5878c2bc7cfd7dfb98a6fe7bc1f366
Steve Anderson <prototype909@msn.com>
2020-08-04 18:24:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Code your solutions in this file
 function writeCards(name, event) {
+  let thankYouCards = []
   for (let i = 0; i < names.lenght; i++) {
     console.log("Thank you, ${name[i]}, for the wonderful ${event} gift!")
   }
