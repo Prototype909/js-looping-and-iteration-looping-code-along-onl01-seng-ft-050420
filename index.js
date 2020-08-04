d0e8c0bb2019e57231e862226991853d6eb5f8e2
Steve Anderson <prototype909@msn.com>
2020-08-04 18:28:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 // Code your solutions in this file
+const names = ['Ada', 'Brendan', 'Ali']
+let event = []
+
 function writeCards(name, event) {
-  let thankYouCards = []
   for (let i = 0; i < names.lenght; i++) {
-    console.log("Thank you, ${name[i]}, for the wonderful ${event} gift!")
+    event[i] = 'Thank you, ${name[i]}, for the wonderful ${event} gift!';
   }
   return thankYouCards
 }
