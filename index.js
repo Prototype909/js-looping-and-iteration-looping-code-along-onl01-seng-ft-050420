6f62809f8214288e0d64c60e7d0bc837d192b9fa
Steve Anderson <prototype909@msn.com>
2020-08-04 18:23:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+  for (let i = 0; i < names.lenght; i++) {
+    console.log("Thank you, ${name[i]}, for the wonderful ${event} gift!")
+  }
+  return thankYouCards
+}
