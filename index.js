38b96c0ccf898779c1ddda6e3a4669d91d678b8f
Steve Anderson <prototype909@msn.com>
2020-08-04 18:42:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-function writeCards(name, event) {
-  let thankYouCards = []
-  for (let i = 0; i < name.lenght; i++) {
-    thankYouCards.push = ('Thank you, ${name[i]}, for the wonderful ${event} gift!');
-  }
-  return thankYouCards;
+
+function writeCards( namesArray, event ) {
+let thankYouCards = []
+for ( let i = 0; i < namesArray.length; i++ ) {
+  thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+}
+return thankYouCards
 }
